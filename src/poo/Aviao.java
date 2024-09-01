c9c0512dcbdac6eb8ac17ffee74ed0d520fbee2f
EmilleFeliciano <emillefeliciano1@gmail.com>
2024-08-31 22:15:22 -0300

    Fundamentos

---
+++ src/poo/Aviao.java
@@ -0,0 +1,14 @@
+package poo;
+
+public class Aviao extends Carro{
+	
+	double envergadura;
+	
+	void aterrizar() {
+		System.out.println("----------_________________");
+	}
+
+	void acelerar () {
+		System.out.println("_____________________ -----------");
+	}
+}
